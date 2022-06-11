cc006b3f83250cebb71f5512564274cff435997e
FalmingosDev <89380775+FalmingosDev@users.noreply.github.com>
2022-06-11 14:23:04 +0530

    11-6-22 2:22 pm

--- NEWOV2/src/app/auth/signup/signup.component.ts
+++ NEWOV2/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    $(document).ready(function() {
+
+      $(".show-password, .hide-password").on('click', function() {
+        var passwordId = $(this).parents('li:first').find('input').attr('id');
+        if ($(this).hasClass('show-password')) {
+          $("#" + passwordId).attr("type", "text");
+          $(this).parent().find(".show-password").hide();
+          $(this).parent().find(".hide-password").show();
+        } else {
+          $("#" + passwordId).attr("type", "password");
+          $(this).parent().find(".hide-password").hide();
+          $(this).parent().find(".show-password").show();
+        }
+      });
+    });
+  }
 
   ngOnInit(): void {
   }
